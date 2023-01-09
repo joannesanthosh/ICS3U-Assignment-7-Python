ecd979d93cf735ec905168aa330ad2b8557f411c
Joanne Santhosh <joanne.santhosh@mths.ca>
2023-01-09 16:22:54 +0000

    update

--- assignment_7.py
+++ assignment_7.py
@@ -5,33 +5,41 @@
 # This program finds the even numbers in a list
 
 
-def even_numbers(list_U):
-    list2 = []
-    loop_counter = 0
+import random
 
-    for single_element in list_U:
-        if loop_counter % 2 == 0:
-            list2.append(single_element)
-        loop_counter += 1
 
-    return list2
+def find_even_numbers(list_of_numbers):
+    even_number = list_of_numbers[0]
+
+    for counter in range(0, len(list_of_numbers)):
+        if list_of_numbers[counter] % 2 == 0:
+            even_number = list_of_numbers[counter]
+
+    return even_number
 
 
 def main():
-   # This function takes the input and produces a list
+    # this function uses an array
 
-    list_U = []
+    my_numbers = []
 
     # input
-    for loop_counter in range(0, 10):
-        single_random_number = random.randint(0, 100)
-        random_numbers.append(single_random_number)
-
-    list2 = odd_indices(list_U)
-
+    for loop_counter in range(0, 100):
+        random_number = random.randint(1, 50)
+        my_numbers.append(random_number)
     print("")
-    print(list_U)
-    print("The elements in odd positions in the list are: {0}".format(list2))
+
+    print("Here are 100 random numbers:")
+
+    for loop_counter in range(0, 100):
+        print("The random number is: {0}".format(my_numbers[loop_counter]))
+
+# calls function
+    even_number = find_even_numbers(random_number)
+    print("")
+    print("The even numbers are {0}".format(even_number))
+
+    print("\nDone.")
 
 
 if __name__ == "__main__":
